da51cb479cc0c27a8c5d769213b9fb2ae585612d
Ananya P <ananyanaran@gmail.com>
2025-02-26 07:44:24 +0530

    added frontend of expenses,budget-plan

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import SignIn from './Pages/Signin';
 import SignUp from './Pages/Signup'
+import Expenses from './Pages/Expenses'
+import BudgetPlan from './Pages/BudgetPlan/BudgetPlan'
 function App() {
   return (
     <BrowserRouter>
     <Routes>
     <Route path="/sign-in" element={<SignIn/>} />
     <Route path="/sign-up" element={<SignUp/>} />
+    <Route path ="/expenses" element={<Expenses/>} />
+    <Route path = "/plan" element={<BudgetPlan/>} />
     </Routes>
     </BrowserRouter>
     
