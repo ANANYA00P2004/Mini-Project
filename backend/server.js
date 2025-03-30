a1238bea4acb036373ec208e07fee4a4f98b094c
Ananya P <119623073+ANANYA00P2004@users.noreply.github.com>
2025-03-30 20:03:06 +0530

    wishlist page done

--- backend/server.js
+++ backend/server.js
@@ -8,23 +8,14 @@ const authRoutes = require("./src/routes/AuthRoutes");
 const futureEventRoutes = require("./src/routes/FutureEventRoute"); // Import future event routes
 const homeRoutes = require("./src/routes/HomeRoute");
 const BudgetPlan = require("./src/routes/BudgetRoutes")
-//const Profile = require("./src/routes/ProfileRoutes")
-//const Wishlist= require("./src/routes/WishRoutes")
 //const Expenses = require("./src/routes/ExpensesRoute")
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: "http://localhost:3000",    // ✅ Allow frontend origin
-  methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
-  allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization",
-}));
-
-
 // Middleware
 app.use(express.json());
-
+app.use(cors());
 
 // PostgreSQL Connection Pool (for direct PostgreSQL usage, though Supabase is preferred)
 const pool = new Pool({
@@ -42,9 +33,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/futureevents", futureEventRoutes);
 app.use("/api/home", homeRoutes);
 app.use("/api/BudgetPlan",BudgetPlan)
-//app.use("/api/profile",Profile)
-//app.use("api/wishlist",Wishlist)
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log('🚀 Server running on http://localhost:${PORT}');
 });
